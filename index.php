f1d398f5b0a9ca03c7532292eb01ae39a0f99870
davidpradaspallares <77846124+davidpradaspallares@users.noreply.github.com>
2021-06-13 03:01:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HOLA</h1>
+    <?php
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
 $empresa = $_POST['mensaje'];
@@ -20,3 +30,5 @@ mail($para, $asunto, utf8_decode($mensaje), $header);
 
 header("Location:index.html");
 ?>
+</body>
+</html>
